d5422110369a526d9f79ce7a382f92dbafc6ff06
toby-dicken <t.dicken2@wlv.ac.uk>
2026-03-06 10:20:37 +0000

    Update config.example.php
    Specified some things

--- config.example.php
+++ config.example.php
@@ -1,6 +1,6 @@
 <?php
-// Copy this file to config.php and fill in your real credentials.
-// Do NOT commit config.php.
+// Copy the contents of this file to config.php and fill in your real credentials.
+// Do NOT commit the real config.php, that is a security risk (though config.php is in gitignore now, so it shouldn't commit).
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'app_user');     // avoid root
