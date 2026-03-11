aec4f771ec049c45366bc74a92c43f8533deafcf
NituDulal <N.dulal2@wlv.ac.uk>
2026-03-11 16:29:17 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,24 @@
+
 <?php
-$mysqli = new mysqli("localhost","2444208","nima2006","db2444208");
-if ($mysqli -> connect_errno) {
-echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-exit();
+// Database 1
+$db1 = new mysqli("localhost","2444208","nima2006","db2444208");
+if ($db1->connect_errno) {
+    echo "Failed to connect to DB1: " . $db1->connect_error;
+    exit();
+}
+
+// Database 2
+$db2 = new mysqli("localhost","2435269","University1039","db2435269");
+if ($db2->connect_errno) {
+    echo "Failed to connect to DB2: " . $db2->connect_error;
+    exit();
+}
+
+// Database 3
+$db3 = new mysqli("localhost","2452032","Neetu@123","db2452032");
+if ($db3->connect_errno) {
+    echo "Failed to connect to DB3: " . $db3->connect_error;
+    exit();
 }
 ?>
+
