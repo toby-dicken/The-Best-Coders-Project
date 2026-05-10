d5f2072b26529142a4be0934a617c0198cef20ab
Toby Dicken <t.dicken2@wlv.ac.uk>
2026-05-11 00:19:51 +0100

    clearing up mmp

--- app,py.py
+++
@@ -1,6 +0,0 @@
-def sum_of_evens(start, end):
-    total = 0
-    for number in range(start, end + 1):
-        if number % 2 == 0:
-            total += number
-    print("Sum of even numbers:", total)
